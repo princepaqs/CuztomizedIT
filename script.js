8aa50ee93927237f1ffffbe10216d1eaf5b5c14a
Prince Louie <20210713m.princepaquiado.bscs@gmail.com>
2025-04-14 13:34:17 +0800

    updates

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function nextSlide(sliderId) {
 
   if (sliderId === 'small-slider') {
     smallIndex = (smallIndex + 1) % totalSlides;
-    slider.style.transform = `translateX(-${smallIndex * (300/ 4)}%)`; // Adjust for width
+    slider.style.transform = `translateX(-${smallIndex * (300/ 4)}%)`;
   } else {
     largeIndex = (largeIndex + 1) % totalSlides;
-    slider.style.transform = `translateX(-${largeIndex * (100 / 10)}%)`; // Adjust for width
+    slider.style.transform = `translateX(-${largeIndex * (100 / 10)}%)`;
   }
 }
 
@@ -22,10 +22,10 @@ function prevSlide(sliderId) {
 
   if (sliderId === 'small-slider') {
     smallIndex = (smallIndex - 1 + totalSlides) % totalSlides;
-    slider.style.transform = `translateX(-${smallIndex * (100 / 4)}%)`; // Adjust for width
+    slider.style.transform = `translateX(-${smallIndex * (100 / 4)}%)`;
   } else {
     largeIndex = (largeIndex - 1 + totalSlides) % totalSlides;
-    slider.style.transform = `translateX(-${largeIndex * (100 / 4)}%)`; // Adjust for width
+    slider.style.transform = `translateX(-${largeIndex * (100 / 4)}%)`;
   }
 }
 
@@ -38,21 +38,19 @@ function prevSlide(sliderId) {
       sidebar.classList.toggle("hidden");
     });
   
-    // Close the sidebar when clicking on a link
+
     sidebarLinks.forEach(link => {
       link.addEventListener("click", function () {
         sidebar.classList.add("hidden");
       });
     });
   
-    // Close the sidebar when clicking outside of it
     document.addEventListener("click", function (event) {
       if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
         sidebar.classList.add("hidden");
       }
     });
-  
-    // Image slider functionality
+
     const sliderImages = document.querySelector(".slider-images");
     const slides = document.querySelectorAll(".slider-image");
     const prevButton = document.querySelector(".slider-button.prev");
@@ -63,7 +61,7 @@ function prevSlide(sliderId) {
     const totalSlides = slides.length;
     const slideInterval = 3000; // Interval time in milliseconds (3 seconds)
   
-    // Array of taglines
+  
     const taglines = [
       "Innovate with Us!",
       "Your Vision, Our Expertise",
@@ -97,10 +95,9 @@ function prevSlide(sliderId) {
     prevButton.addEventListener("click", prevSlide);
     nextButton.addEventListener("click", nextSlide);
   
-    // Auto-slide functionality
     setInterval(nextSlide, slideInterval);
   
-    // Initialize tagline on page load
+
     updateTagline();
   });
   
